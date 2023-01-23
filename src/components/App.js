c163da6e0017a5359eb2215ba84d0c9cd3bc0065
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-01-23 22:20:15 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
+import { Route, Switch} from "react-router-dom";
+import NavBar from "./NavBar";
+import Actors from "./Actors";
+import Home from "./Home";
+
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+ 
+  return (
+    <div>
+      <NavBar/>
+      <switch>
+        <Route path="/movies">
+          <Movies/>
+        </Route>
+        <Route path="/directors">
+          <Directors/>
+        </Route>
+        <Route path="/actors">
+          <Actors/>
+        </Route>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </switch>
+    </div>
+  );
 }
 
 export default App;
